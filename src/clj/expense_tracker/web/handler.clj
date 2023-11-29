34bae12d10ba401c361b510ca609b87d2a0b9087
Almos Zediu <almos.zediu@fabritglobal.com>
2023-11-29 17:02:40 +0200

    Returning something

--- src/clj/expense_tracker/web/handler.clj
+++ src/clj/expense_tracker/web/handler.clj
@@ -1,10 +1,14 @@
 (ns expense-tracker.web.handler
   (:require
-   [integrant.core :as ig]))
+   [integrant.core :as ig]
+   [reitit.ring :as ring]))
 
 (defmethod ig/init-key :handler/ring
-  [_ _]
-  (fn [req]
-    {:status 200
-     :body "OK"})
+  [_ {:keys [router]}]
+   (ring/ring-handler router (ring/create-default-handler))
   )
+
+(defmethod ig/init-key :router/core
+  [_ {:keys [routes]}]
+  (ring/router
+   routes))
---
+++ src/clj/expense_tracker/web/routes/api.clj
@@ -0,0 +1,13 @@
+(ns expense-tracker.web.routes.api
+  (:require
+   [integrant.core :as ig]
+   [reitit.swagger :as swagger]
+   [reitit.swagger-ui :as swagger-ui]))
+
+(defmethod ig/init-key :router/routes
+  [_ {:keys [db]}]
+  (println "Initializing routes")
+  [["/api" (fn [_] {:status 200 :body "Hello"})]
+   ["" {:no-doc true}
+    ["/swagger.json" {:get (swagger/create-swagger-handler)}]
+    ["/api-docs/*" {:get (swagger-ui/create-swagger-ui-handler)}]]])
